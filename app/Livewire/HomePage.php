63a1d58dfe4b4b8ca90d47a39dec1b586bb3c9f5
Luft21 <jfebryan15@gmail.com>
2025-06-11 17:41:04 +0700

    add random laptop fade animation on home page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -4,15 +4,21 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Laptop;
 
 class HomePage extends Component
 {
     public function render()
     {
+        $images = Laptop::inRandomOrder()->limit(5)->pluck('gambar', 'nama')->toArray();
         if (Auth::check()) {
-            return view('livewire.home-page')->layout('components.layouts.app');
+            return view('livewire.home-page', [
+                'laptopImages' => $images,
+            ])->layout('components.layouts.app');
         } else {
-            return view('livewire.home-page')->layout('components.layouts.guest.app');
+            return view('livewire.home-page', [
+                'laptopImages' => $images,
+            ])->layout('components.layouts.guest.app');
         }
     }
 }
--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -26,27 +26,25 @@
     x-data="{
         active: 0,
         images: [
-        { src: '{{ asset('images/rog-flow.png') }}', label: 'ROG Flow Z13' },
-        { src: '{{ asset('images/acer-nitro.png') }}', label: 'Acer Nitro' },
-        { src: '{{ asset('images/asus-tuf.png') }}', label: 'Asus TUF' },
-        { src: '{{ asset('images/hp-omen.png') }}', label: 'HP Omen' },
-        { src: '{{ asset('images/lenovo-legion.png') }}', label: 'Lenovo Legion' }
+            @foreach($laptopImages as $nama => $gambar)
+                { src: '{{ asset('storage/alternatif-images/' . $gambar) }}', label: '{{ $nama }}' },
+            @endforeach
         ],
         init() {
-        setInterval(() => {
-            this.next();
-        }, 3000); // Change every 3 seconds
+            setInterval(() => {
+                this.next();
+            }, 5000); // Ganti setiap 5 detik
         },
         prev() {
-        this.active = (this.active === 0) ? this.images.length - 1 : this.active - 1;
+            this.active = (this.active === 0) ? this.images.length - 1 : this.active - 1;
         },
         next() {
-        this.active = (this.active === this.images.length - 1) ? 0 : this.active + 1;
+            this.active = (this.active === this.images.length - 1) ? 0 : this.active + 1;
         }
-    }" 
+    }"
     x-init="init()"
     class="flex flex-col items-center max-w-md mx-auto"
-    >
+>
     <div class="relative w-120 p-2 rounded-tr-3xl rounded-bl-3xl h-90 overflow-hidden">
         <template x-for="(image, index) in images" :key="index">
             <img
@@ -57,7 +55,6 @@
             />
         </template>
     </div>
-
     <div class="relative h-10 mt-3">
         <template x-for="(image, index) in images" :key="'label-' + index">
             <h5
@@ -67,24 +64,5 @@
                 <span x-text="image.label"></span>
             </h5>
         </template>
-
-    <!-- Buttons
-    <div class="flex space-x-4 mt-3">
-        <button 
-        @click="prev()" 
-        class="px-4 py-2 text-white rounded hover:bg-gray-600"
-        aria-label="Previous image"
-        >
-        <i class="fas fa-chevron-left text-xl"></i>
-        </button>
-        <button 
-        @click="next()" 
-        class="px-4 py-2 text-white rounded hover:bg-gray-600"
-        aria-label="Next image"
-        >
-        <i class="fas fa-chevron-right text-xl"></i>
-        </button>
-    </div> -->
     </div>
-
 </div>
